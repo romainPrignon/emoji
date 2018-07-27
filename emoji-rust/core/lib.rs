c3673f90dca74a7fd0a6d657598c575c97279505
romainprignon <pro.rprignon@gmail.com>
2018-07-27 14:21:42 +0200

    :sparkles: (rust) add http client

--- emoji-rust/core/lib.rs
+++ emoji-rust/core/lib.rs
@@ -1,7 +1,40 @@
-// fn do_query() {}
+extern crate hyper;
+
+use hyper::Client;
+use hyper::rt::{Future};
+
+fn do_query(word: String) -> impl Future<Item=(), Error=hyper::Error> { // WTF &'static str ??
+    let http_client = Client::new();
+
+    let url = "http://emoji.getdango.com/api/emoji".parse().unwrap();
+    
+    
+    return http_client
+        .get(url)
+        .map(|res| {
+            println!("Response: {}", "ici");
+            println!("Response: {}", res.status());
+        })
+    
+    
+    
+    
+    // if word == "love" {
+    //     return "❤"
+    // } else {
+    //     return "the emoji for the word"
+    // }
+}
 
 // fn filter_best_emoji() {}
 
-fn emoji() {
-    println!("Hello, world!")
+fn emoji(word: String) -> () {
+    do_query(word)
+
+    // let res = do_query(word);
+    //println!("{}", res.status());
+}
+
+fn main() {
+    emoji(String::from("love"))
 }
