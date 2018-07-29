410b5a27b7e59c0a0bd50ddf686c12f03f44ec79
romainprignon <pro.rprignon@gmail.com>
2018-07-29 22:45:26 +0200

    :art: (rust) use docker

--- emoji-rust/core/lib.rs
+++ emoji-rust/core/lib.rs
@@ -1,40 +1,36 @@
-extern crate hyper;
+// extern crate hyper;
 
-use hyper::Client;
-use hyper::rt::{Future};
+// use hyper::Client;
+// use hyper::rt::{Future};
 
-fn do_query(word: String) -> impl Future<Item=(), Error=hyper::Error> { // WTF &'static str ??
-    let http_client = Client::new();
+// fn do_query(word: String) -> impl Future<Item=(), Error=hyper::Error> { // WTF &'static str ??
+//     let http_client = Client::new();
 
-    let url = "http://emoji.getdango.com/api/emoji".parse().unwrap();
+//     let url = "http://emoji.getdango.com/api/emoji".parse().unwrap();
     
     
-    return http_client
-        .get(url)
-        .map(|res| {
-            println!("Response: {}", "ici");
-            println!("Response: {}", res.status());
-        })
-    
-    
-    
-    
-    // if word == "love" {
-    //     return "❤"
-    // } else {
-    //     return "the emoji for the word"
-    // }
-}
+//     return http_client
+//         .get(url)
+//         .map(|res| {
+//             println!("Response: {}", "ici");
+//             println!("Response: {}", res.status());
+//         })
+// }
 
 // fn filter_best_emoji() {}
 
-fn emoji(word: String) -> () {
-    do_query(word)
-
-    // let res = do_query(word);
-    //println!("{}", res.status());
+fn emoji(word: String) -> &'static str {
+    // let res = do_query(word)
+    // return filter_best_emoji(res)
+    
+    if word == "love" {
+        return "❤";
+    } else {
+        return "the emoji for the word";
+    }
 }
 
 fn main() {
-    emoji(String::from("love"))
+    let emo = emoji(String::from("love"));
+    println!("{}", emo);
 }
