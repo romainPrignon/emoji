d007b17077c4a3f66e817af41e289fa56c0cf191
romainprignon <pro.rprignon@gmail.com>
2018-08-28 23:30:34 +0200

    :art: (ts) add typescript version

---
+++ emoji-ts/src/Core.ts
@@ -0,0 +1,5 @@
+export default class Core {
+    run(word: string) {
+        return "<3"
+    }
+} 
---
+++ emoji-ts/src/main.ts
@@ -0,0 +1,5 @@
+import Core from './Core'
+
+const core = new Core()
+
+console.log(core.run(process.argv.slice(2)))
