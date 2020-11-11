52ba4dc95adf051fc754473a5ae632641e38e8ae
romainprignon <pro.rprignon@gmail.com>
2020-11-11 15:33:29 +0100

    🐛 use new api

--- emoji-py/src/core/main.py
+++ emoji-py/src/core/main.py
@@ -5,17 +5,19 @@ from argparse import Namespace
 def Core(http_client):
     
     def do_query(word):
-        return http_client.get("http://emoji.getdango.com/api/emoji", params = {"q": word})
+        access_key = "c19560f67ed78510d1fa453f5609665c9538934d"
+        return http_client.get("https://emoji-api.com/emojis", params = {"search": word, "access_key": access_key})
 
 
     def parse_json(res):
-        return res.json()["results"]
+        # return res.json()["results"]
+        return res.json()[0]
 
 
-    def filter_best_emoji(results):
-        maxEmoji = max(results, key = lambda r: r["score"])
+    def filter_best_emoji(res):
+        # maxEmoji = max(results, key = lambda r: r["score"])
 
-        return maxEmoji["text"]
+        return res["character"]
 
 
     def run(word):
